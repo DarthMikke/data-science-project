7a3008328a8217886ea3e72a30d683c42de3f49e
Michal Jan Warecki <mwarecki1@gmail.com>
2022-02-02 17:28:18 +0100

    Lagre søk i resultat-mappa.

---
+++ deltid/main.py
@@ -0,0 +1,50 @@
+import pandas as pd
+import json
+from datetime import datetime
+from Arbeidsplassen import stilling, sok
+
+search_query = {
+    'occupationFirstLevels[]': 'Helse%20og%20sosial',
+    'occupationSecondLevels[]': 'Helse%20og%20sosial.Helse',
+    'size': '50'
+}
+"https://arbeidsplassen.nav.no/stillinger/stilling/673e8258-8959-440f-b387-d97bbc4f83c2"
+"https://arbeidsplassen.nav.no/stillinger/stilling/334f73d8-0e3f-476c-a61e-2d858d420ff4"
+
+timestamp = datetime.utcnow().isoformat()[:-7]
+total = 200  # TODO: set denne variablen til 0 for produksjon
+fetched = -1
+all_hits = []
+
+# resolved = pd.read_csv('resolved.csv')
+while fetched < total:
+    if fetched > 0:
+        search_query['from'] = fetched
+
+    response = sok(search_query)
+
+    if total == 0:
+        total = response['hits']['total']['value']
+
+    hits = [x['_source'] for x in response['hits']['hits']]
+
+    hits = [
+        {
+            'reference': x['reference'],
+            'county': x['locationList'][0]['county'],
+            'municipal': x['locationList'][0]['municipal'],
+            "postal_code": x['locationList'][0]['postalCode'],
+            "businessName": x['businessName'],
+            "source": x['source'],
+            "title": x['title'],
+            "uuid": x['uuid']
+        } for x in hits
+    ]
+
+    all_hits += hits
+    fetched += len(hits)
+
+hits_df = pd.read_json(json.dumps(all_hits), orient='records')
+print(hits_df.info())
+print(hits_df[:10]['uuid'])
+hits_df.to_csv('resultat/search_results-{}.csv'.format(timestamp))
