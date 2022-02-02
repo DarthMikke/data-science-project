d398642c2228d45bd83bd5deb4890b739dc398f4
Michal Jan Warecki <mwarecki1@gmail.com>
2022-02-02 18:31:36 +0100

    Lagre direkte i pandas.DataFrame.

--- deltid/get_details.py
+++ deltid/get_details.py
@@ -1,34 +1,49 @@
 import pandas as pd
 import json
 from datetime import datetime
-from Arbeidsplassen import stilling
+import Arbeidsplassen
 
 FILENAME = "resultat/search_results-2022-02-02T16:24:12.csv"
 # Kor mange stillingar skal hentast? Set til 0 for å hente alle.
-TOTAL = 10
+TOTAL = 50
 
 timestamp = datetime.utcnow().isoformat()[:-7]
 hits_df = pd.read_csv(FILENAME)
 
 print(hits_df.info())
-uuids = [x for x in hits_df.head(TOTAL)['uuid']]
-openings = [stilling(x) for x in uuids]
-openings = [
-    {
-        'reference': x['reference'],
-        "source": x['source'],
-        "sourceurl": x['properties']['sourceurl'],
-        "uuid": x['uuid'],
-        "postal_code": x['locationList'][0]['postalCode'],
-        'county': x['locationList'][0]['county'],
-        'municipal': x['locationList'][0]['municipal'],
-        "businessName": x['businessName'],
-        "title": x['title'],
-        "extent": x['properties']['extent'],
-        "count": x['properties']['positioncount'],
-        "engagementtype": x['properties']['engagementtype'],
-    } for x in openings
-]
+uuids = hits_df.head(TOTAL)['uuid'] if TOTAL < len(hits_df) else hits_df['uuid']
+columns = ['reference', 'source', 'sourceurl', 'uuid', 'postal_code', 'county', 'municipal',
+           'businessName', 'title', 'extent', 'count', 'engagementtype']
+openings = pd.DataFrame([], columns=columns)
+
+total = len(uuids)
+for i in range(total):
+    x = uuids[i]
+    print(f"Hentar {i}/{total}: {x}.")
+    stilling = Arbeidsplassen.stilling(x)['_source']
+    new_row = {
+        'reference': stilling['reference'],
+        "source": stilling['source'],
+        "uuid": x,
+        "postal_code": stilling['locationList'][0]['postalCode'],
+        'county': stilling['locationList'][0]['county'],
+        'municipal': stilling['locationList'][0]['municipal'],
+        "businessName": stilling['businessName'],
+        "title": stilling['title'],
+        "extent": stilling['properties']['extent'],
+        "count": stilling['properties']['positioncount'],
+        "engagementtype": stilling['properties']['engagementtype']
+            if 'engagementtype' in stilling['properties'].keys()
+            else None,
+        'sourceurl': stilling['properties']['sourceurl']
+            if 'sourceurl' in stilling['properties'].keys()
+            else None,
+        'jobpercentage': stilling['properties']['jobpercentage']
+            if 'jobpercentage' in stilling['properties'].keys()
+            else None,
+    }
+
+    openings = openings.append(new_row, ignore_index=True)
 
 openings = pd.read_json(json.dumps(openings), orient='records')
 print(openings.info())
