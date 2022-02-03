795173a35ab7d5d34bd381bb08f20505277ef3af
Michal Jan Warecki <mwarecki1@gmail.com>
2022-02-03 09:19:57 +0100

    Feilsikker henting av data.

--- deltid/get_details.py
+++ deltid/get_details.py
@@ -3,7 +3,7 @@ import json
 from datetime import datetime
 import Arbeidsplassen
 
-FILENAME = "resultat/search_results-2022-02-02T16:24:12.csv"
+FILENAME = "resultat/search_results-2022-02-02T18:37:15.csv"
 # Kor mange stillingar skal hentast? Set til 0 for å hente alle.
 TOTAL = 0
 
@@ -20,28 +20,36 @@ total = len(uuids)
 for i in range(total):
     x = uuids[i]
     print(f"Hentar {i}/{total}: {x}.")
-    stilling = Arbeidsplassen.stilling(x)['_source']
-    new_row = {
-        'reference': stilling['reference'],
-        "source": stilling['source'],
-        "uuid": x,
-        "postal_code": stilling['locationList'][0]['postalCode'],
-        'county': stilling['locationList'][0]['county'],
-        'municipal': stilling['locationList'][0]['municipal'],
-        "businessName": stilling['businessName'],
-        "title": stilling['title'],
-        "extent": stilling['properties']['extent'],
-        "count": stilling['properties']['positioncount'],
-        "engagementtype": stilling['properties']['engagementtype']
-            if 'engagementtype' in stilling['properties'].keys()
-            else None,
-        'sourceurl': stilling['properties']['sourceurl']
-            if 'sourceurl' in stilling['properties'].keys()
-            else None,
-        'jobpercentage': stilling['properties']['jobpercentage']
-            if 'jobpercentage' in stilling['properties'].keys()
-            else None,
-    }
+    try:
+        stilling = Arbeidsplassen.stilling(x)['_source']
+        new_row = {
+            'reference': stilling['reference'],
+            "source": stilling['source'],
+            "uuid": x,
+            "postal_code": stilling['locationList'][0]['postalCode'],
+            'county': stilling['locationList'][0]['county'],
+            'municipal': stilling['locationList'][0]['municipal'],
+            "businessName": stilling['businessName'],
+            "title": stilling['title'],
+            "extent": stilling['properties']['extent']
+                if 'extent' in stilling['properties'].keys()
+                else None,
+            "count": stilling['properties']['positioncount']
+                if 'positioncount' in stilling['properties'].keys()
+                else None,
+            "engagementtype": stilling['properties']['engagementtype']
+                if 'engagementtype' in stilling['properties'].keys()
+                else None,
+            'sourceurl': stilling['properties']['sourceurl']
+                if 'sourceurl' in stilling['properties'].keys()
+                else None,
+            'jobpercentage': stilling['properties']['jobpercentage']
+                if 'jobpercentage' in stilling['properties'].keys()
+                else None,
+        }
+    except Exception as e:
+        with open("details.log", "a") as fh:
+            fh.write(f"\n{e}")
 
     openings = openings.append(new_row, ignore_index=True)
 
